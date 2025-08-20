e77ea3ed844140f946f961d52f2ff7424cb90125
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-08-20 20:15:19 +0330

    🐛 all objs added to a list

--- 1_5.js
+++ 1_5.js
@@ -1,13 +1,15 @@
 function transList(array) {
     let obj = {}
+    let final = []
 
     array.forEach(element => {
         let elementLen = element.length
-        obj[elementLen] = element
+        final.push(obj[elementLen] = element)
     });
 
     return obj
 }
 
 let array = ["hi", "I've been", "waiting", "for", "you", "to skeem", "my code"]
+
 console.log(transList(array))
